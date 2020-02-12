0b2a43bcad0c1aa22a2d9be4b87d2afcb7d2987b
adrtrn <adrian.s.tran@gmail.com>
2020-02-12 10:58:42 -0800

    implements bsaic crud operations for express server

--- server/index.js
+++ server/index.js
@@ -2,35 +2,41 @@ const express = require('express');
 const port = 3002;
 const path = require('path');
 
-const retrieve = require('../database/retrieve.js'); // module for query a specific house in DB
+const retrieve = require('../database/retrieve.js');
 
-// set up server
 var app = express();
 app.set('port', port);
 
-// parsing
 app.use(express.urlencoded({'extended': true}));
-
-// specify the directory of static files
 app.use('/', express.static(path.join(__dirname, '../client/dist')));
 
-// response to request
-app.get('/api/summary/data/:id', (req, res) =>{
+
+app.post('/api/summary/data/new', (req, res) => {
+})
+
+app.put('/api/summary/data/:id', (req, res) => {
+})
+
+app.delete('/api/summary/data/:id', (req, res) => {
+})
+
+
+app.get('/api/summary/data/:id', (req, res) => {
   var {id} = req.params;
   retrieve(id, (err, data) =>{
-    if (err) { // if error occures, send status code of 500
-      console.log(err); 
+    if (err) {
+      console.log(err);
       res.send(500);
     } else {
-      if(data){ // send data back to client if retrieved
-        res.jsonp(data);    
-      } else { // if query succeed but no data retrieve, send message to client about issue
+      if (data) {
+        res.jsonp(data);
+      } else {
         res.send('no house with such id value');
       }
     }
   });
-  
 })
+
 
 // start server
 app.listen(port, () => {
