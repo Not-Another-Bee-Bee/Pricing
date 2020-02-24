6da889a8b664cdcb7eb9e31d9c79f6a445be9337
adrtrn <adrian.s.tran@gmail.com>
2020-02-24 11:33:17 -0800

    implements postgres with current ui   - old api requests are replaced with new postgres db routes.   - ui is currently grabbing correct data and rendering accurately.   - implements crud operations for properties

--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const port = 3002;
 const path = require('path');
-const queries = require('../db/queries');
-
+const db = require('../db/queries');
 
 let app = express();
 app.set('port', port);
@@ -10,14 +9,10 @@ app.set('port', port);
 app.use(express.urlencoded({'extended': true}));
 app.use('/', express.static(path.join(__dirname, '../client/dist')));
 
-app.post('/api/v1/properties/new', (req, res) => {})
-
-app.get('/api/v1/properties/:id', (req, res) => {})
-
-app.put('/api/v1/properties/:id', (req, res) => {})
-
-app.delete('/api/v1/properties/:id', (req, res) => {})
-
+app.get('/api/v1/properties/:id', db.getPropertyById);
+app.post('/api/v1/properties/new', db.addNewProperty);
+app.put('/api/v1/properties/:id/edit', db.editPropertyById);
+app.delete('/api/v1/properties/:id', db.deletePropertyById);
 
 // start server
 app.listen(port, () => {
